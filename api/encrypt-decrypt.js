f1410df6193332245ff352d8fe56672ad5109dd7
zoldikk <167327016+zoldikk@users.noreply.github.com>
2024-09-29 14:46:42 +0100

    Update encrypt-decrypt.js

--- api/encrypt-decrypt.js
+++ api/encrypt-decrypt.js
@@ -1,3 +1,16 @@
+export function Encrypt(number) {
+    number = parseInt(number);
+    const encodedBytes = [];
+    while (true) {
+        let byte = number & 0x7F;
+        number >>= 7;
+        if (number) byte |= 0x80;
+        encodedBytes.push(byte);
+        if (!number) break;
+    }
+    return Buffer.from(encodedBytes).toString('hex');
+}
+
 export function Decrypt(encodedBytes) {
     encodedBytes = Buffer.from(encodedBytes, 'hex');
     let number = 0, shift = 0;
@@ -7,5 +20,5 @@ export function Decrypt(encodedBytes) {
         shift += 7;
         if (!(byte & 0x80)) break;
     }
-    return number >>> 0;  // Ensures the result is always a positive number
+    return number;
 }
