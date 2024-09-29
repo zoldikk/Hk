ef3636f2920bddace378b26ca7c66e2e69cfee4c
zoldikk <167327016+zoldikk@users.noreply.github.com>
2024-09-29 14:37:44 +0100

    Create encrypt-decrypt.js

---
+++ api/encrypt-decrypt.js
@@ -0,0 +1,24 @@
+export function Encrypt(number) {
+    number = parseInt(number);
+    const encodedBytes = [];
+    while (true) {
+        let byte = number & 0x7F;
+        number >>= 7;
+        if (number) byte |= 0x80;
+        encodedBytes.push(byte);
+        if (!number) break;
+    }
+    return Buffer.from(encodedBytes).toString('hex');
+}
+
+export function Decrypt(encodedBytes) {
+    encodedBytes = Buffer.from(encodedBytes, 'hex');
+    let number = 0, shift = 0;
+    for (const byte of encodedBytes) {
+        const value = byte & 0x7F;
+        number |= value << shift;
+        shift += 7;
+        if (!(byte & 0x80)) break;
+    }
+    return number;
+      }
